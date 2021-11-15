8beae67209c994f182cc88cbf302dadfd4732d59
Coker Oluwasegun <cokeroluwasegun@outlook.com>
2021-11-15 09:37:51 -0800

    Created 10-check_cycle.c, lists.h

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "list.h"
+/**
+* check_cycle - check for cycle in a linked list
+* @list: the list
+* Return: has cycle or not
+*/
+int check_cycle(listint_t *list)
+{
+listint_t *f = list;
+listint_t *s = list;
+while (f && s && f->next)
+{
+s = s->next;
+f = (f->next)->next;
+if (s == f)
+return (1);
+}
+return (0);
+}
