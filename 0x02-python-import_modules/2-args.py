b128223cd5054241bc2a6d4457a275b8e759d54c
Coker Oluwasegun <cokeroluwasegun@outlook.com>
2021-11-18 09:17:15 -0800

    Update 2-argv.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,7 +5,7 @@ args = sys.argv
 l = len(args)
 if l > 1:
     if l == 2:
-        print("{:d} argument".format(l - 1))
+        print("{:d} arguments:".format(l - 1))
         print("{:d}: {:s}".format(1, args[1]))
     else:
         print("{:d} agruments:".format(l - 1))
