50fbb3f8ae6e2915e2bd0406ef49e384f00a79e1
Coker Oluwasegun <cokeroluwasegun@outlook.com>
2021-11-18 16:55:33 -0800

    Update 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -9,7 +9,7 @@ if length > 1:
         print("{:d}: {:s}".format(1, argv[1]))
     else:
         print("{:d} arguments:".format(length - 1))
-        for i in range(1, l):
+        for i in range(1, length):
             print("{:d}: {:s}".format(i, argv[i]))
 else:
     print("{:d} arguments.".format(length - 1))
