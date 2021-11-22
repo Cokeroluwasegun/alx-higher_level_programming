bf3d38c9dc7ed0b76f3a9b870be2d2527f492f74
Coker Oluwasegun <cokeroluwasegun@outlook.com>
2021-11-21 19:03:24 -0800

    Updated and fixed bugs in 9 - 12

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,7 +3,7 @@ def max_integer(my_list=[]):
     if len(my_list) == 0:
         return None
     a = my_list[0]
-    for 1 in my_list:
+    for i in my_list:
         if i > a:
             a = i
     return a        
