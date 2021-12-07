3e86f003cd49916daf455ac0aef550b50b803454
Coker Oluwasegun <cokeroluwasegun@outlook.com>
2021-12-07 10:39:01 -0800

    Created 3-main.py and 3-square.py file

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+class Square:
+    """Square that defines a square by: (based on 2-square.py)"""
+    def __init__(self, size=0):
+        if isinstance(size, int):
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = size
+        else:
+            raise TypeError("size must be an integer")
+
+    def area(self):
+        """that returns the current square area"""
+        return (self.__size ** 2)
