11a8e1019983f91bb7b87d7283848e380cd12c57
Coker Oluwasegun <cokeroluwasegun@outlook.com>
2021-12-13 11:04:29 -0800

    Created 3-rectangle.py file

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 """
 Rectangle module.
-This module provides a rectangle class with methods and attributes.
+This module provides a rectangle class.
 """
 
 
 class Rectangle:
-    """Rectangle class attribs: width, height  method: area, perimeter"""
+    """Rectangle class with attributes and methods.
+    Attributes : height and width.
+    methods: area, perimeter and str.
+    """
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
@@ -38,9 +41,21 @@ class Rectangle:
             self.__height = value
 
     def area(self):
+        """method that returns area of rectangle"""
         return self.__height * self.__width
 
     def perimeter(self):
+        """method that returns perimeter of rectangle"""
         h = self.__height
         w = self.__width
         return 0 if h is 0 or w is 0 else (h + w) * 2
+
+    def __str__(self):
+        """method that returns printable rectangle"""
+        printrec = ""
+        if self.__height is 0 or self.__width is 0:
+            return printrec
+        for i in range(self.__height):
+            printrec += ("#" * self.__width)
+            printrec += "\n"
+        return printrec[:-1]
