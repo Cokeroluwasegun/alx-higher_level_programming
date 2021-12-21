3db09823417752da9efed9f65db7c4580069e6d8
Coker Oluwasegun <cokeroluwasegun@outlook.com>
2021-12-21 06:40:44 -0800

    created update

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,17 +1,12 @@
-#!/usr/bin/python3
+d#!/usr/bin/python3
 """
    function that reads a text file (UTF8) and prints it to stdout:
 """
 
 
-def number_of_lines(filename=""):
+def read_file(filename=""):
     """
-    function that returns the number of lines
-    in a file
-    @filename: the name of the file
+       function that reads a text file (UTF8) and prints it to stdout:
     """
-    with open(filename) as f:
-        i = 0
-        for line in f:
-            i += 1
-    return i
+    with open(filename, "r", encoding='utf-8') as f:
+        print(f.read(), end='')
