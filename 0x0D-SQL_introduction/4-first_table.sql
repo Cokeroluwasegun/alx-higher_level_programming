691782abfae904c890e8c17a4638e2f268aa4c3b
Coker Oluwasegun <cokeroluwasegun@outlook.com>
2022-02-01 06:18:51 -0800

    Created 4-first_table.sql

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,2 +1,5 @@
 -- script creates a table with id and name
-CREATE TABLE IF NOT EXIST first_table(id INT, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS first_table(
+        id INT,
+	name VARCHAR(256)
+);
