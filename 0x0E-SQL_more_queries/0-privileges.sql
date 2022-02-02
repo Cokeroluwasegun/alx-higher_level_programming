7383a87317332ee23884086950a3585d93f1b4be
Coker Oluwasegun <cokeroluwasegun@outlook.com>
2022-02-02 08:11:57 -0800

    Created README and 0-privileges.sql file

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,4 @@
+-- script that lists all privileges of the MYSQL users
+-- script that lists all privileges of user "user_0d_1"
+SHOW GRANTS FOR user_0d_1@localhost;
+SHOW GRANTS FOR user_0d_2@localhost;
