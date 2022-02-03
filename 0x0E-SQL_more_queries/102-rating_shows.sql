f209b4a55c8a9849960a41f243df2c5d51baddc3
Coker Oluwasegun <cokeroluwasegun@outlook.com>
2022-02-02 17:35:06 -0800

    Created from 13 - 103 file

---
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -0,0 +1,5 @@
+-- script that lists all shows from hbtn_0d_tvshows_rate by their rating.
+-- Each record should display: tv_shows.title - rating sum
+SELECT t.title, SUM(tsr.rate) AS rating FROM tv_shows t
+LEFT JOIN tv_show_ratings tsr ON t.id = tsr.show_id
+GROUP BY t.title ORDER BY rating DESC;
