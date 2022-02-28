afbf5e5c7849ef21a110466f89ff64dbee5e8e3b
Coker Oluwasegun <cokeroluwasegun@outlook.com>
2022-02-28 11:46:12 -0800

    Edited 1-multi_languages.js file

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,4 @@
 #!/usr/bin/node
 //script that prints 3 lines
-const myVar = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-let i = 0;
-for (i = 0; i < myVar.length; i++) {
-  console.log(myVar[i]);
-}
+const a = 'C is fun\nPython is cool\nJavascript is amazing';
+console.log(a);
