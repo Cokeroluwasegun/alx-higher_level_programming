1989d4b6d881b88b183c8458310ea3f536101c95
Coker Oluwasegun <cokeroluwasegun@outlook.com>
2022-02-28 14:26:46 -0800

    Created 3-value_argument.js file

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,12 @@
 #!/usr/bin/node
 // a script that prints the first argument passed to it
-if (typeof process.argv[2] === 'undefined') {
+let checkArgs = false;
+process.argv.forEach((val, index) => {
+  if (index === 2) {
+    console.log(`${val}`);
+    checkArgs = true;
+  }
+});
+if (checkArgs === false) {
   console.log('No argument');
-} else {
-  console.log(process.argv[2]);
 }
