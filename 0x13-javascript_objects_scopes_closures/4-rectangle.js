8b5aaa046df3280d69b80bdcafeca0ea80380699
Coker Oluwasegun <cokeroluwasegun@outlook.com>
2022-03-01 08:40:09 -0800

    Created 4-rectangle.js file

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,29 @@
+#!/usr/bin/node
+// Script that creates empty class
+class Rectangle {
+    constructor (w, h) {
+	if (w > 0 && h > 0) {
+	    this.width = w;
+	    this.height =h;
+	}
+    }
+    // print
+    print () {
+	for (let i = 0; i < this.height; i++) {
+	    for (let j = 0; j < this.width; j++) {
+		process.stdout.write('X');
+	    }
+	process.stdout.write('\n');
+	}
+    }
+    // rotate
+    rotate () {
+	[this.width, this.height] = [this.height, this.width];
+    }
+    // double
+    double () {
+	this.width *= 2;
+	this.height *= 2;
+    }
+}
+module.exports = Rectangle;
