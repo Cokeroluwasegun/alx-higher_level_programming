f59101731973397f6081a63e12079ab06093902c
Coker Oluwasegun <cokeroluwasegun@outlook.com>
2022-03-01 08:29:39 -0800

    Created 3-rectangle.js file

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+// Script that creates empty class
+class Rectangle {
+    constructor (w, h) {
+	if (w > 0 && h > 0) {
+	    this.width = w;
+	    this.height = h;
+	}
+    }
+    // print
+    print () {
+	for (let i = 0; i< this.width; i++) {
+	    for (let j = 0; j < this.height; i++) {
+		process.stdout.write('X');
+	    }
+	    process.stdout.write('\n');
+	}
+    }
+}
+module.exports = Rectangle;
