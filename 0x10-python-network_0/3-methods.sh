2049897e19207214dbc5697c7d404dbe3fb9f8d7
Coker Oluwasegun <cokeroluwasegun@outlook.com>
2022-03-17 19:02:57 -0700

    Created 3-methods.sh

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that takes in a URL nd display all HTTP methods the server accept
-curl-sI "$1" | grep "Allow:" | cut -d " " -f2-
+curl -sI $1 | grep Allow | cut -d ' ' -f2-
