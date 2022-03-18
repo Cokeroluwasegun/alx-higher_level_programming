e2be42cf8fe435ce5032a3dab62d98ee66d430f0
Coker Oluwasegun <cokeroluwasegun@outlook.com>
2022-03-18 09:47:35 -0700

    Created 1-hbtn_header.py and edited 0-hbtn_status.py file

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-""" Python script that fetches https://intranet.hbtn.io/status """
 import urllib.request
-
-with urllib.request.urlopen('https://intranet.hbtn.io/status') as f:
-    req = f.read()
-    print('Body response:')
-    print('\t- type:', type(req))
-    print('\t- content:', req)
-    print('\t- utf8 content:', req.decode('ascii'))
+""" Python script that fetches https://intranet.hbtn.io/status """
+if __name__ == "__main__":
+    url = urllib.request.Request('https://intranet.hbtn.io/status')
+    with urllib.request.urlopen(url) as f:
+        req = f.read()
+        print('Body response:')
+        print('\t- type:', type(req))
+        print('\t- content:', req)
+        print('\t- utf8 content:', req.decode('ascii'))
