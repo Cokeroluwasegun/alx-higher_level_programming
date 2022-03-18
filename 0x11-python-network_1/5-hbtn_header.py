b4b3260e9068dea12266807cfbd5b4e84682338b
Coker Oluwasegun <cokeroluwasegun@outlook.com>
2022-03-18 10:30:34 -0700

    Created 3-100 file

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Python script """
+import requests
+from sys import argv
+def execute():
+    """ Intermediate method for checker. """
+    req = requests.get(argv[1])
+    print(req.headers.get("X-Request-Id"))
+    
+if __name__ == "__main__":
+    execute()
---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Python script """
+import requests
+from sys import argv
+def execute():
+    """ Intermediate method for checker. """
+    req = requests.get(argv[1])
+    stat = req.status_code
+    if stat >= 400:
+        print("Error code:", req.status_code)
+    else:
+        print(req.text)
+
+
+if __name__ == "__main__":
+    execute()
