c380438a4497c35ee13792027be2cefa55eb5cff
Coker Oluwasegun <cokeroluwasegun@outlook.com>
2022-03-17 19:18:25 -0700

    Created 6-main.py  6-peak.txt 6-peak.py file

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def find_peak(list_of_integers):
+    """
+    find the peak element in a list
+    """
+
+    if list_of_integers == []:
+        return None
+    arr = list_of_integers
+    for i in range(1, len(arr)-1):
+        if arr[i] > arr[i-1] and arr[i] > arr[i+1]:
+            return arr[i]
+    return max(arr[0], arr[-1])
