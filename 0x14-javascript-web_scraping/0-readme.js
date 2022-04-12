966bcdac39703fad2dd48923570db21f57c6aba7
Oluwasegun Coker <cokeroluwasegun@outlook.com>
2022-04-12 18:10:21 +0100

    Created directory and 0-readme.js file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,15 +1,6 @@
 #!/usr/bin/node
-// Script that reads a file and prints its content
-
-
 const fs = require('fs');
-const file = process.argv[2];
-const encodeing = 'utf-8';
-
-fs.readfile(file, encoding, function(err,data) {
-    if (err) {
-	console.log(err):
-    }else {
-	console.log(data);
-    }
+fs.readFile(process.argv[2], (err, data) => {
+  if (err) console.log(err);
+  else console.log(data.toString());
 });
